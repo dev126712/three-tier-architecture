c73725335f04519449e40dbcb8db827d99fd14fc
Alexandre St-fort <solonoobhacker6767+@gmail.com>
2025-11-10 15:24:59 -0500

    Update alb_sg.tf

--- alb_sg.tf
+++ alb_sg.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "public-alb-security-group" {
 
 
   egress {
-    escription = "allows egress from everywhare" 
+    description = "allows egress from everywhare" 
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
