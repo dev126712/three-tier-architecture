e1e81a2e821a63ec09f99079a750011431ef6181
Alexandre St-fort <solonoobhacker6767+@gmail.com>
2025-11-10 15:56:31 -0500

    Update baston-host.tf

--- baston-host.tf
+++ baston-host.tf
@@ -8,10 +8,11 @@ resource "aws_security_group" "baston-host-alb-security-group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["78.67.5.54/32"]
   }
 
   egress {
+    description = "allows egress to everywhere using https"
     from_port   = 0
     to_port     = 0
     protocol    = "443"
@@ -25,11 +26,20 @@ resource "aws_security_group" "baston-host-alb-security-group" {
 
 resource "aws_instance" "bastion-host" {
   ami                         = var.amis
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.baston_host_keypair.key_name
   security_groups             = [aws_security_group.baston-host-alb-security-group.id]
   subnet_id                   = aws_subnet.public-subnet-bastion-host.id
+
+  metadata_options {
+    http_tokens              = "required" # Forces IMDSv2
+    http_endpoint            = "enabled"
+    instance_metadata_tags   = "enabled"
+  }
+   monitoring {
+    enabled = true
+  }
 
   tags = {
     Name = "Bastion Host"
